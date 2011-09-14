e1466d72ffd1c205d0179b4f917423325fc6dbbd
Seth Woodworth <seth@sethish.com>
2011-09-14 18:06:53 -0400

    typo in database setup

--- blogish/settings.py
+++ blogish/settings.py
@@ -11,7 +11,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psychopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
